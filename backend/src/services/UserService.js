9db692123f0655e90398f441dd201877a84ebbee
Kishan <bang.siki2009@gmail.com>
2022-07-29 20:38:34 +0545

    Create Userservice

---
+++ backend/src/services/UserService.js
@@ -0,0 +1,34 @@
+const User = require('../database/schemas/userSchema');
+
+const createUser = async (payload) => {
+    try {
+        const user = await User.create(payload);
+        return user;
+    } 
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
+const getUser = async (email) => {
+    try {
+        const user = await User.findOne({email});
+        return user;
+    } 
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const getAllUsers = async () => {
+    try {
+        const users = await User.find();
+        return users;
+    } 
+    catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {createUser, getUser};
