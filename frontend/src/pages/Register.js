22b3c1251c6d2711ff7aaac3c3aaa9748aca1c60
Sujan Chhetri Poudel <sujan.poudel210@gmail.com>
2022-08-09 20:54:33 +0545

    Add validation in register form

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import {Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 import useRegister from '../hooks/useRegister';
 import styles from './Register.module.css';
 
@@ -10,51 +9,37 @@ const Register = () => {
     const [phone, setPhone] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-    const {register} = useRegister();
+    const { register, error } = useRegister();
 
-    const handleSubmit = (e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(name, email, phone, password);
-        const user = {name, email, phone, password};
+        // console.log(name, email, phone, password);
+        const user = { name, email, phone, password };
 
         register(user);
-        // axios.post('',user,{
-        //     headers:{
-        //         "Content-Type": "application/json"
-        //     }
-        // })
-        // .then((res)=>{
-        //     navigate('/');
-        // })
-        // .catch((err)=>{
-        //     console.log(err.message);
-        // })
+    };
 
-        
-
-    }
-
-    return ( 
+    return (
         <div className={styles.register}>
             <h2>Register</h2>
             <form onSubmit={handleSubmit} className={styles.form}>
                 <label>Name</label>
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
-                <div className={styles.error}></div>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+                {error.name && <div className={styles.error}>{error.name}</div>}
                 <label>Email</label>
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-                <div className={styles.error}></div>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                {error.email && <div className={styles.error}>{error.email}</div>}
                 <label>Phone</label>
-                <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} required/>
-                <div className={styles.error}></div>
+                <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+                {error.phone && <div className={styles.error}>{error.phone}</div>}
                 <label>Password</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-                <div className={styles.error}></div>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                {error.password && <div className={styles.error}>{error.password}</div>}
                 <button>Register</button>
             </form>
             <p>Already Registered?<Link to="/login"> Login!</Link></p>
         </div>
-     );
-}
- 
+    );
+};
+
 export default Register;
